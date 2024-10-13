1835eb6593bc4697f1b8f5c0240d5ccecdf41445
Meet Shah <meetcshah19@gmail.com>
2024-10-13 22:05:02 +0530

    update postgres

--- postgres/entrypoint.sh
+++ postgres/entrypoint.sh
@@ -6,8 +6,18 @@ mkdir ~/data
 
 chmod 750 ~/data
 
-/usr/lib/postgresql/15/bin/initdb -D ~/data
+touch ~/pgpass
 
-/usr/lib/postgresql/15/bin/pg_ctl -D ~/data
+echo $PGPASSWORD
 
+echo $PGPASSWORD > ~/pgpass
+
+/usr/lib/postgresql/15/bin/initdb -D ~/data -U $PGUSER --pwfile=/home/nonroot/pgpass
+
+echo "unix_socket_directories = '/home/nonroot/data'" >> ~/data/postgresql.conf
+echo "listen_addresses = '*'" >> ~/data/postgresql.conf
+echo "host    all             all             0.0.0.0/0               md5" >> ~/data/pg_hba.conf
+echo "host    all             all             ::/0                    md5" >> ~/data/pg_hba.conf
+
+/usr/lib/postgresql/15/bin/postgres -D ~/data
 
